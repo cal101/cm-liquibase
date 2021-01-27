7234a5d30dbdbcf334767a3a59a86e373fe84840
cal101 <cal101@users.noreply.github.com>
2021-01-27 11:11:30 +0000

    refactor: erefactor/AutoRefactor - Remove useless modifiers
    AutoRefactor cleanup 'RedundantModifiersCleanUp' applied by erefactor:
    
    Sorts modifiers.
    Also removes modifiers implied by the context:
    - "static" and "abstract" for interface,
    - "public", "static" and "final" for interface fields,
    - "public" and "abstract" for interface methods,
    - "final" for private methods,
    - "final" for parameters in interface method declarations,-
    "protected" modifier for final class not inherited members.
    
    For AutoRefactor see https://github.com/JnRouvignac/AutoRefactor
    For erefactor see https://github.com/cal101/erefactor

--- liquibase-integration-tests/src/test/java/liquibase/test/DatabaseTest.java
+++ liquibase-integration-tests/src/test/java/liquibase/test/DatabaseTest.java
@@ -3,5 +3,5 @@ package liquibase.test;
 import liquibase.database.Database;
 
 public interface DatabaseTest {
-    public void performTest(Database database) throws Exception;
+    void performTest(Database database) throws Exception;
 }
